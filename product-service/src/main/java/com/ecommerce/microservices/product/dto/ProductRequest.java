9be746b64dbdeb2729e6665aff371507cd47ec10
Tushar Gupta <tushargupta@Tushars-MBP.lan>
2024-12-16 00:01:50 -0600

    SB-1.Product Service

---
+++ product-service/src/main/java/com/ecommerce/microservices/product/dto/ProductRequest.java
@@ -0,0 +1,7 @@
+package com.ecommerce.microservices.product.dto;
+
+import java.math.BigDecimal;
+
+public record ProductRequest(String id, String name, String description, BigDecimal price) {
+
+}
---
+++ product-service/src/main/java/com/ecommerce/microservices/product/service/ProductService.java
@@ -0,0 +1,36 @@
+package com.ecommerce.microservices.product.service;
+
+import com.ecommerce.microservices.product.dto.ProductRequest;
+import com.ecommerce.microservices.product.dto.ProductResponse;
+import com.ecommerce.microservices.product.model.Product;
+import com.ecommerce.microservices.product.repository.ProductRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+@Slf4j
+public class ProductService {
+    private final ProductRepository productRepository;
+
+    public Product createProduct(ProductRequest productRequest){
+        Product product = Product.builder()
+                .name(productRequest.name())
+                .description(productRequest.description())
+                .price(productRequest.price())
+                .build();
+        productRepository.save(product);
+        log.info("Product Saved");
+        return product;
+    }
+
+    public List<ProductResponse> getAllProducts() {
+        return productRepository.findAll()
+                .stream()
+                .map(product -> new ProductResponse(product.getId(), product.getName(), product.getDescription(), product.getPrice()))
+                .toList();
+    }
+}
